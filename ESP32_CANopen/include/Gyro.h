ffb86166c336bee680dd179129d60474a5631958
Alexander Miller <xXAM22Xx@users.noreply.github.com>
2020-07-24 09:52:42 +0200

    Cleanup

--- ESP32_CANopen/include/Gyro.h
+++ ESP32_CANopen/include/Gyro.h
@@ -1,3 +1,26 @@
+/**
+ * @file Gyro.h
+ * @author Alexander Miller (alexander.miller@fh-dortmund.de)
+ * @author Mathias Parys    (mathias.parys@fh-dortmund.de)
+ * @brief Gyro
+ * @version 0.1
+ * @date 2020-07-24
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #ifndef GYRO_H_
 #define GYRO_H_
 
--- ESP32_CANopen/include/modul_config.h
+++ ESP32_CANopen/include/modul_config.h
@@ -1,16 +1,28 @@
 /**
- * @file hatox_config.h
+ * @file modul_config.h
  * @author Alexander Miller (alexander.miller@fh-dortmund.de)
+ * @author Mathias Parys    (mathias.parys@fh-dortmund.de)
  * @brief Configuration File
  * @version 0.1
- * @date 2019-09-13
+ * @date 2020-07-24
  * 
- * @copyright Copyright (c) 2019
+ * @copyright Copyright (c) 2020
  * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
-#ifndef HATOX_CONFIG_H
-#define HATOX_CONFIG_H
+#ifndef MODUL_CONFIG_H
+#define MODUL_CONFIG_H
 
 //####  MAIN CONFIG  ####
 #define BOOT_WAIT 2000
@@ -48,4 +60,4 @@
 #define PIN_SMI_MDC 23  /** PHY ETH MDC Pinnumber */
 #define PIN_SMI_MDIO 18 /** PYH ETH MDIO Pinnumber */
 
-#endif /* HATOX_CONFIG_H */
+#endif /* MODUL_CONFIG_H */
